a2f93eae7a2de73ff3800c9658e692da544d917f
jsanchezperuga <66560125+jsanchezperuga@users.noreply.github.com>
2022-03-24 22:34:44 -0300

    Initial commit

---
+++ Assets/Ejercicio01.cs
@@ -0,0 +1,21 @@
+﻿//Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+//valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla. Después mostrá el
+//resultado de multiplicarlo por sí mismo.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
